da99ec18a282db24c3e83fa4b1dd5ec10c5b00e9
Poulvilho <pv.paulovictor.vp@gmail.com>
2022-03-16 23:00:05 -0300

    🚧  adjust on user tests

--- tests/user.spec.js
+++ tests/user.spec.js
@@ -1,60 +1,53 @@
 const request = require('supertest');
 const app = require('../src/app');
+
 describe('User tests', () => {
-  /*
+
   it('Teste Registro correto', async (done) => {
     const response = await request(app)
       .post('/api/user/register')
       .send({
-          name: 'Paulo Teste',
-          email: 'pv.pauloteste.vp@gmail.com',
+          name: 'Teste',
+          email: 'teste@teste.com',
           password: '#Teste123',
       });
 
     expect(response.status).toBe(200);
-    expect(response.body.data).toHaveProperty('id');
+    expect(response.body).toHaveProperty('id');
     done();
   });
-  */
+  
   it('Teste Login correto', async (done) => {
     const response = await request(app)
       .post('/api/user/login')
       .send({
-          email: 'pv.pauloteste.vp@gmail.com',
+          email: 'teste@teste.com',
           password: '#Teste123',
       });
 
     expect(response.status).toBe(200);
-    expect(response.body.data).toHaveProperty('id');
+    expect(response.body).toHaveProperty('id');
     done();
   });
   
-  /*
   it('Teste Edita correto', async (done) => {
     const response1 = await request(app)
       .post('/api/user/login')
       .send({
-          email: 'pv.pauloteste.vp@gmail.com',
+          email: 'teste@teste.com',
           password: '#Teste123',
       });
 
     const response2 = await request(app)
-      .put(`/api/user/${response2.body.data.id}`)
+      .put(`/api/user/${response1.body.id}`)
       .send({
-        name: 'Paulo Victor Lopes',
-        email: 'pv.pauloteste.vp@gmail.com',
+        name: 'Teste Atualizado',
+        email: 'teste@teste.com',
         password: '#Teste123',
       });
-    
-    const response3 = await request(app)
-      .post('/api/user/login')
-      .send({
-          email: 'pv.pauloteste.vp@gmail.com',
-          password: '#Teste123',
-      });
 
-    expect(response.status).toBe(200);
-    expect(response1.body.data.name) != expect(response3.body.data.name)
+    expect(response2.status).toBe(200);
+    expect(response2.body.data).toBe(1);
     done();
   });
   
@@ -62,15 +55,14 @@ describe('User tests', () => {
     const response1 = await request(app)
       .post('/api/user/login')
       .send({
-          email: 'pv.pauloteste.vp@gmail.com',
+          email: 'teste@teste.com',
           password: '#Teste123',
       });
 
     const response2 = await request(app)
-      .delete(`/api/user/${response2.body.data.id}`);
+      .delete(`/api/user/${response1.body.id}`);
 
     expect(response2.status).toBe(200);
     done();
   });
-  */
 });
