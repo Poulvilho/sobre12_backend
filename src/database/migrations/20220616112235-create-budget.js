5d4fc7b5323cd9d825e5732d363295ba0d1783b0
Paulo Victor Lopes <paulo.lopes@grupoorion.com.br>
2022-08-02 21:36:52 -0300

    Update migration order

---
+++ src/database/migrations/20220616112235-create-budget.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('budgets', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      value: {
+        allowNull: false,
+        type: Sequelize.DOUBLE,
+      },
+      category: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      subcategory: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      dtbudget: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      trip: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        references: {
+          model: {
+            tableName: 'trips'
+          },
+          key: 'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('budgets', {
+      cascade: true,
+    });
+  }
+};
